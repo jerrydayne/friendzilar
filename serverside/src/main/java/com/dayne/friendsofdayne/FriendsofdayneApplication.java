51ba586818abe20005d0bdb72b819fb48800503f
jerrydayne <jerrydaexplorer@gmail.com>
2024-08-14 00:40:15 +0100

    Initialization

---
+++ serverside/src/main/java/com/dayne/friendsofdayne/FriendsofdayneApplication.java
@@ -0,0 +1,13 @@
+package com.dayne.friendsofdayne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsofdayneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsofdayneApplication.class, args);
+	}
+
+}
---
+++ serverside/src/test/java/com/dayne/friendsofdayne/FriendsofdayneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayne.friendsofdayne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsofdayneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
